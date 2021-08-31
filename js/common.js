baf1589448aa0db30cf8f43903a4ed8f55de9d7a
Walkinded <labunskij1997@gmail.com>
2021-08-31 13:08:36 +0300

    done top-seller-tabs section

--- js/common.js
+++ js/common.js
@@ -81,3 +81,20 @@ $(function() {
     $('.header__mobile').fadeOut();
   });
 });
+
+/* ==================== TABS ==================== */ 
+$(document).ready(function($) {
+  $('.tab_content').hide();
+  $('.tab_content:first').show();
+  $('.tabs li:first').addClass('active');
+  $('.tabs li').click(function(event) {
+    event.preventDefault();
+    $('.tabs li').removeClass('active');
+    $(this).addClass('active');
+    $('.tab_content').hide();
+
+    var selectTab = $(this).find('a').attr("href");
+
+    $(selectTab).fadeIn();
+  });
+});
